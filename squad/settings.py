2ca1d0e932f19c887a011da31df65a5cfecbc921
yukesh570 <yukesh.maha10@gmail.com>
2025-11-26 10:55:17 +0545

    changes

--- squad/settings.py
+++ squad/settings.py
@@ -107,12 +107,24 @@ WSGI_APPLICATION = 'squad.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sms',
+        'USER': 'yukesh',
+        'PASSWORD': 'android18)',
+        'HOST': 'localhost',  # or the container name if using Docker
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
