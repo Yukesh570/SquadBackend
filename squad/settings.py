219cabffeffbb82415752b111d68a47ad161ca72
yukesh570 <yukesh.maha10@gmail.com>
2025-12-09 18:29:22 +0545

    setting chagne add media_url

--- squad/settings.py
+++ squad/settings.py
@@ -172,6 +172,12 @@ USE_I18N = True
 USE_TZ = True
 AUTH_USER_MODEL = "squadServices.User"
 
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
