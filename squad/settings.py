f477ca48509f11fc781ce8b5c0bb95b263e66e9f
yukesh570 <yukesh.maha10@gmail.com>
2026-01-21 11:13:04 +0545

    rrestart

--- squad/settings.py
+++ squad/settings.py
@@ -181,7 +181,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = "/app/staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
