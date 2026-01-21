4e58a8b1758b2be4dc40b0834a7c9763daabda84
yukesh570 <yukesh.maha10@gmail.com>
2026-01-21 11:13:50 +0545

    restart

--- squad/settings.py
+++ squad/settings.py
@@ -57,10 +57,10 @@ SPECTACULAR_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
