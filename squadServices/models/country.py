8bf9df8446d5d70e6e47ba46038e038d4fc2e3ee
yukesh570 <yukesh.maha10@gmail.com>
2026-02-27 11:39:00 +0545

    restart

--- squadServices/models/country.py
+++ squadServices/models/country.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=100)  # User-friendly name
+    name = models.CharField(max_length=100)
     countryCode = models.CharField(max_length=100)
     MCC = models.CharField(max_length=100)
     isDeleted = models.BooleanField(default=False)
