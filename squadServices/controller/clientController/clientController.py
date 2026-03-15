49f2302ff2ff107c34a004e2a1ebf03c1dffc9ed
yukesh570 <yukesh.maha10@gmail.com>
2026-03-15 23:23:00 +0545

    CompanyaddedToClient

--- squadServices/controller/clientController/clientController.py
+++ squadServices/controller/clientController/clientController.py
@@ -42,7 +42,7 @@ class ClientFilter(ExtendedFilterSet):
             "name": ["exact", "icontains", "isnull"],
             "status": ["exact", "icontains", "isnull"],
             "route": ["exact", "icontains", "isnull"],
-            "country__name": ["exact", "icontains", "isnull"],
+            "company__name": ["exact", "icontains", "isnull"],
             "smppUsername": ["exact", "icontains", "isnull"],
             "paymentTerms": ["exact", "icontains", "isnull"],
             "creditLimit": ["exact", "gt", "lt", "range", "isnull"],
